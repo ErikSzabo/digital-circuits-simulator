9273e8e701ff51d82fa60424fcd509f1ae16e2e0
Erik Szabó <erik1szabo1@gmail.com>
2020-04-09 14:01:21 +0200

    Started Device specific command

--- src/hu/erik/digitalcircuits/cli/commands/DeviceCmd.java
+++ src/hu/erik/digitalcircuits/cli/commands/DeviceCmd.java
@@ -1,10 +1,10 @@
 package hu.erik.digitalcircuits.cli.commands;
 
 import hu.erik.digitalcircuits.cli.DeviceMap;
+import hu.erik.digitalcircuits.cli.DeviceType;
 import hu.erik.digitalcircuits.errors.clierror.NotEnoughArgsException;
 
 public class DeviceCmd extends Command {
-
 
     public DeviceCmd(String name) {
         super(name);
@@ -12,6 +12,22 @@ public class DeviceCmd extends Command {
 
     @Override
     public void action(DeviceMap storage, String[] cmd) throws NotEnoughArgsException {
+        // Format
+        // <devicetype> <name> <uniqe method> <args...>
 
+        if(cmd.length < 3) throw new NotEnoughArgsException(cmd[0], 4, cmd.length - 1);
+
+        switch (cmd[0]) {
+            case DeviceType.JUNCTION:
+                break;
+            case DeviceType.CIRCUITBOX:
+                break;
+            case DeviceType.SWITCH:
+                break;
+            case DeviceType.POWER:
+                break;
+            default:
+                break;
+        }
     }
 }
